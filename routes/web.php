aa851d33a793c171ba59bb989ee17c8aa6189d6c
Andrés Felipe Cano <andresfe98@gmail.com>
2019-03-20 13:10:45 -0500

    Integración de laravel Telescope
    Implementaci´no de laravel telescope para tener un seguimiento de todos los movimientos de la aplicación.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 Route::resource('medicamento', 'MedicamentoController');
 
 Route::resource('verificacion', 'VerificacionController');
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
